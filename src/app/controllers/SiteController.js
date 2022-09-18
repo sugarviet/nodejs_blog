5a6ecfccbfdeee6bba3d6471e41800e9aa51006b
Viet Dang <vietdanghoang1705@gmail.com>
2022-09-18 15:22:37 +0700

    Read From DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,36 @@
-class SiteController {
-    // [GET] /home
-    index(req, res) {
-        res.render('home');
-    }
+const Course = require("../model/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 
-    // [GET] /search
-    search(req, res) {
-        res.render('search');
-    }
+class SiteController {
+  // [GET] /home
+  index(req, res) {
+    // Course.find({}, function(err, courses, next){
+    //     if(!err){
+    //     res.json(courses)
+    //     }
+    //     else{
+    //         next(err);
+    //         // res.status(400).json({error: 'ERROR'})
+    //     }
+    // });
+
+    Course.find({})
+      .then((courses) => {
+        res.render("home", { courses: multipleMongooseToObject(courses) });
+      })
+      .catch((err) => next(err));
+
+    // res.json({
+    //     name:'test'
+    // });
+
+    // res.render('home');
+  }
+
+  // [GET] /search
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
